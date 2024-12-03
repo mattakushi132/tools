a8bb0aeab820dd74ed1d90438a8f198fd57f8440
mattakushi132 <177023913+mattakushi132@users.noreply.github.com>
2024-12-03 20:44:38 +0300

    Added the setting of duplicate characters + Changing the order of the elements

--- password_generator.py
+++ password_generator.py
@@ -24,37 +24,21 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.setFixedSize(QSize(480, 280))
+        self.setFixedSize(QSize(480, 240))
         self.setWindowTitle('Password generator')
         
+        self.primary_font = QFont('sans-serif', 12, 700)
+        self.secondary_font = QFont('sans-serif', 10, 500)
         
-        primary_font = QFont('sans-serif', 12, 700)
-        secondary_font = QFont('sans-serif', 10, 500)
+        self.setFont(self.secondary_font)
         
         top_left_alignment = (Qt.AlignmentFlag.AlignTop
                               | Qt.AlignmentFlag.AlignLeft)
         
         
-        self.password_field = QLineEdit(self)
-        self.password_field.setPlaceholderText('the generated password '
-                                               'will be here...')
-        self.password_field.textChanged.connect(
-            self.password_field_text_changed
-        )
-        
-        self.password_field_copy = QPushButton()
-        self.password_field_copy.setText('Copy')
-        self.password_field_copy.setEnabled(False)
-        self.password_field_copy.clicked.connect(self.copy_password)
-        
-        password_field_layout = QHBoxLayout()
-        password_field_layout.addWidget(self.password_field)
-        password_field_layout.addWidget(self.password_field_copy)
-        
-        
         self.characters_label = QLabel()
         self.characters_label.setText('Characters:')
-        self.characters_label.setFont(primary_font)
+        self.characters_label.setFont(self.primary_font)
         
         self.lowercase = QCheckBox()
         self.lowercase.setText(string.ascii_lowercase)
@@ -76,29 +60,16 @@ class MainWindow(QMainWindow):
         self.punctuation.setChecked(False)
         self.punctuation.stateChanged.connect(self.checkbox_state_changed)
         
-        characters_grid = QGridLayout()
-        characters_grid.addWidget(self.lowercase, 0,0)
-        characters_grid.addWidget(self.digits, 0,1)
-        characters_grid.addWidget(self.uppercase, 1,0)
-        characters_grid.addWidget(self.punctuation, 1,1)
+        characters_grid_layout = QGridLayout()
+        characters_grid_layout.addWidget(self.lowercase, 0,0)
+        characters_grid_layout.addWidget(self.digits, 0,1)
+        characters_grid_layout.addWidget(self.uppercase, 1,0)
+        characters_grid_layout.addWidget(self.punctuation, 1,1)
         
         characters_layout = QVBoxLayout()
         characters_layout.addWidget(self.characters_label)
-        characters_layout.addLayout(characters_grid)
+        characters_layout.addLayout(characters_grid_layout)
         characters_layout.setAlignment(top_left_alignment)
-        
-        
-        self.exclude_label = QLabel()
-        self.exclude_label.setText('Exclude some characters:')
-        self.exclude_label.setFont(secondary_font)
-        
-        self.exclude_characters = QLineEdit()
-        self.exclude_characters.setPlaceholderText('exclude some characters')
-        
-        exclude_layout = QVBoxLayout()
-        exclude_layout.addWidget(self.exclude_label)
-        exclude_layout.addWidget(self.exclude_characters)
-        exclude_layout.setAlignment(top_left_alignment)
         
         
         self.password_lenght = QSlider()
@@ -114,7 +85,6 @@ class MainWindow(QMainWindow):
         self.password_lenght_label.setText(
             f'Password lenght: {self.password_lenght.value()}'
         )
-        self.password_lenght_label.setFont(secondary_font)
         
         password_lenght_layout = QVBoxLayout()
         password_lenght_layout.addWidget(self.password_lenght_label)
@@ -122,24 +92,137 @@ class MainWindow(QMainWindow):
         password_lenght_layout.setAlignment(top_left_alignment)
         
         
-        self.button = QPushButton()
-        self.button.setText('Generate password')
-        self.button.clicked.connect(self.generate_password)
+        self.duplicate_characters = QSlider()
+        self.duplicate_characters.setMinimum(1)
+        self.duplicate_characters.setMaximum(4)
+        self.duplicate_characters.setValue(2)
+        self.duplicate_characters.setOrientation(Qt.Orientation.Horizontal)
+        self.duplicate_characters.valueChanged.connect(
+            self.duplicate_characters_changed
+        )
+        
+        self.duplicate_characters_label = QLabel()
+        self.duplicate_characters_label.setText(
+            f'Duplicate characters: {self.duplicate_characters.value()}'
+        )
+        
+        duplicate_characters_layout = QVBoxLayout()
+        duplicate_characters_layout.addWidget(self.duplicate_characters_label)
+        duplicate_characters_layout.addWidget(self.duplicate_characters)
+        duplicate_characters_layout.setAlignment(top_left_alignment)
+        
+        
+        password_settings_layout = QHBoxLayout()
+        password_settings_layout.addLayout(password_lenght_layout, 1)
+        password_settings_layout.addSpacing(10)
+        password_settings_layout.addLayout(duplicate_characters_layout)
+        
+        
+        self.exclude_label = QLabel()
+        self.exclude_label.setText('Exclude some characters')
+        
+        self.exclude_characters = QLineEdit()
+        self.exclude_characters.setPlaceholderText('Example: 123456QwertY')
+        self.exclude_characters.textChanged.connect(self.check_the_generation)
+        
+        exclude_layout = QHBoxLayout()
+        exclude_layout.addWidget(self.exclude_label)
+        exclude_layout.addWidget(self.exclude_characters)
+        exclude_layout.setAlignment(top_left_alignment)
+        
+        
+        self.password_label = QLabel()
+        self.password_label.setText('Password')
+        
+        self.password_field = QLineEdit(self)
+        self.password_field.setPlaceholderText(
+            'The generated password will be here...'
+        )
+        self.password_field.textChanged.connect(
+            self.password_field_text_changed
+        )
+        
+        self.password_field_copy = QPushButton()
+        self.password_field_copy.setText('Copy')
+        self.password_field_copy.setEnabled(False)
+        self.password_field_copy.clicked.connect(self.copy_password)
+        
+        password_field_layout = QHBoxLayout()
+        password_field_layout.addWidget(self.password_label)
+        password_field_layout.addWidget(self.password_field)
+        password_field_layout.addWidget(self.password_field_copy)
+        
+        
+        self.gen_password_btn = QPushButton()
+        self.gen_password_btn.setText('Generate password')
+        self.gen_password_btn.clicked.connect(self.generate_password)
         
         
         password_layout = QVBoxLayout()
-        password_layout.addLayout(password_field_layout)
         password_layout.addLayout(characters_layout)
+        password_layout.addSpacing(4)
+        password_layout.addLayout(password_settings_layout)
+        password_layout.addSpacing(4)
         password_layout.addLayout(exclude_layout)
-        password_layout.addLayout(password_lenght_layout)
-        password_layout.addWidget(self.button)
+        password_layout.addSpacing(8)
+        password_layout.addLayout(password_field_layout)
+        password_layout.addWidget(self.gen_password_btn)
         
         container = QWidget()
         container.setLayout(password_layout)
         
         self.setCentralWidget(container)
     
-    def generate_password(self):
+    def password_field_text_changed(self):
+        text = self.password_field.text()
+        
+        if list(filter(lambda x: x != ' ', text)):
+            self.password_field_copy.setEnabled(True)
+        else:
+            self.password_field_copy.setEnabled(False)
+    
+    def copy_password(self):
+        password = self.password_field.text()
+        
+        if password:
+            QApplication.clipboard().setText(password)
+    
+    def checkbox_state_changed(self):
+        self.update_symbols()
+        self.check_the_generation()
+    
+    def check_the_generation(self):
+        self.update_symbols()
+        
+        gen_chars_lenght = len(self.symbols)*self.duplicate_characters.value()
+        password_lenght = self.password_lenght.value()
+        
+        if gen_chars_lenght >= password_lenght:
+            self.gen_password_btn.setEnabled(True)
+            self.gen_password_btn.setText('Generate password')
+            self.gen_password_btn.setStyleSheet('color: black;')
+        else:
+            self.gen_password_btn.setEnabled(False)
+            self.gen_password_btn.setText(
+                'Too many excludes / Not enough characters to generate'
+            )
+            self.gen_password_btn.setStyleSheet('color: red;')
+        
+        self.gen_password_btn.setFont(self.secondary_font)
+    
+    def password_lenght_changed(self):
+        self.check_the_generation()
+        self.password_lenght_label.setText(
+            f'Password lenght: {self.password_lenght.value()}'
+        )
+    
+    def duplicate_characters_changed(self):
+        self.check_the_generation()
+        self.duplicate_characters_label.setText(
+            f'Duplicate characters: {self.duplicate_characters.value()}'
+        )
+    
+    def update_symbols(self):
         all_symbols = ''
         
         if self.lowercase.isChecked():
@@ -151,58 +234,31 @@ class MainWindow(QMainWindow):
         if self.punctuation.isChecked():
             all_symbols += self.punctuation.text()
         
-        symbols = ''
+        self.symbols = ''
+        
         for ch in all_symbols:
             if ch not in self.exclude_characters.text():
-                symbols += ch
+                self.symbols += ch
+    
+    def generate_password(self):
+        self.update_symbols()
         
         password = ''
         while len(password) < self.password_lenght.value():
-            password_symbol = random.choice(symbols)
+            random_symbol = random.choice(self.symbols)
             
-            if password.lower().count(password_symbol.lower()) < 2:
-                password += password_symbol
+            symbol_counts = password.lower().count(random_symbol.lower())
+            duplicate_chars_count = self.duplicate_characters.value()
+            
+            if symbol_counts < duplicate_chars_count:
+                password += random_symbol
         
         self.password_field.setText(password)
-    
-    def password_lenght_changed(self):
-        self.password_lenght_label.setText(
-            f'Password lenght: {self.password_lenght.value()}'
-        )
-    
-    def copy_password(self):
-        text = self.password_field.text()
-        
-        if text:
-            QApplication.clipboard().setText(text)
-    
-    def password_field_text_changed(self):
-        text = self.password_field.text()
-        
-        if list(filter(lambda x: x != ' ', text)):
-            self.password_field_copy.setEnabled(True)
-        else:
-            self.password_field_copy.setEnabled(False)
-    
-    def checkbox_state_changed(self):
-        is_checked_lowercase = self.lowercase.isChecked()
-        is_checked_uppercase = self.uppercase.isChecked()
-        is_checked_digits = self.digits.isChecked()
-        is_checked_punctuation = self.punctuation.isChecked()
-        
-        if (is_checked_lowercase or is_checked_uppercase
-            or is_checked_digits or is_checked_punctuation):
-            self.button.setEnabled(True)
-        else:
-            self.button.setEnabled(False)
 
-def application():
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     
     window = MainWindow()
     window.show()
     
     sys.exit(app.exec())
-
-if __name__ == '__main__':
-    application()
