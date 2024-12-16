d978debfc72e1ade27d3bd2dea6685902ac6fd1a
mattakushi132 <177023913+mattakushi132@users.noreply.github.com>
2024-12-16 20:05:21 +0300

    Add: Random color generator; Remove: Unnecessary widgets.

--- converter.py
+++ converter.py
@@ -8,15 +8,12 @@ from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
     QTabWidget,
-    QCheckBox,
     QComboBox,
     QLabel,
     QLineEdit,
-    QVBoxLayout,
     QHBoxLayout,
     QGridLayout,
     QPushButton,
-    QSlider,
     QWidget,
 )
 
@@ -45,14 +42,12 @@ class ColorConverter(QMainWindow):
         self.user_input = QLineEdit()
         self.user_input.setPlaceholderText('(171, 205, 239)')
         self.user_input.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.user_input.setMaxLength(15)
         self.user_input.textChanged.connect(self.user_input_text_changed)
         
-        self.preview_color_label = QLabel()
-        self.preview_color_label.setStyleSheet(
-            'background-color: rgb(171,205,239);'
-            'border: 1px solid gray;'
-        )
-        self.preview_color_label.setContentsMargins(30,0,0,0)
+        gen_rand_color = QPushButton()
+        gen_rand_color.setText('Random color')
+        gen_rand_color.clicked.connect(self.generate_random_color)
         
         self.label_convert_to = QLabel()
         self.label_convert_to.setText('HEX:')
@@ -71,6 +66,13 @@ class ColorConverter(QMainWindow):
             Qt.TextInteractionFlag.TextSelectableByMouse
         )
         
+        self.preview_color_label = QLabel()
+        self.preview_color_label.setStyleSheet(
+            'background-color: rgb(171,205,239);'
+            'border: 1px solid gray;'
+        )
+        self.preview_color_label.setContentsMargins(30,0,0,0)
+        
         copy_button = QPushButton()
         copy_button.setText('Copy')
         copy_button.clicked.connect(self.copy_color)
@@ -79,10 +81,11 @@ class ColorConverter(QMainWindow):
         layout = QGridLayout()
         layout.addWidget(self.color_format_combobox, 0,0)
         layout.addWidget(self.user_input, 0,1)
-        layout.addWidget(self.preview_color_label, 0,3, 6,1)
-        layout.addWidget(self.label_convert_to, 2,0)
-        layout.addWidget(self.converted_color_label, 2,1)
-        layout.addWidget(copy_button, 3,0, 3,2)
+        layout.addWidget(gen_rand_color, 0,2)
+        layout.addWidget(self.label_convert_to, 1,0)
+        layout.addWidget(self.converted_color_label, 1,1)
+        layout.addWidget(self.preview_color_label, 1,2, 3,1)
+        layout.addWidget(copy_button, 2,0, 2,2)
         layout.setAlignment(Qt.AlignmentFlag.AlignVCenter)
         
         container = QWidget()
@@ -148,7 +151,7 @@ class ColorConverter(QMainWindow):
                         'background-color: black;'
                         'border: 1px solid red;'
                     )
-                    self.converted_color_label.setText('?.?.?')
+                    self.converted_color_label.setText('(?, ?, ?)')
         else:
             if self.color_format == 'RGB':
                 self.preview_color_label.setStyleSheet(
@@ -162,6 +165,26 @@ class ColorConverter(QMainWindow):
                     'border: 1px solid gray;'
                 )
                 self.converted_color_label.setText('(171, 205, 239)')
+    
+    def generate_random_color(self):
+        color: str = ''
+        rgb_color_range: tuple = (0, 255)
+        hex_color_symbols = 'abcdef' + string.digits
+        
+        match self.color_format:
+            case 'RGB':
+                r = random.randint(*rgb_color_range)
+                g = random.randint(*rgb_color_range)
+                b = random.randint(*rgb_color_range)
+                
+                color = f'({r}, {g}, {b})'
+                
+                self.user_input.setText(color)
+            case 'HEX':
+                while len(color) < 6:
+                    color += random.choice(hex_color_symbols)
+                
+                self.user_input.setText(f'#{color}')
     
     def copy_color(self):
         color_code = self.converted_color_label.text()
